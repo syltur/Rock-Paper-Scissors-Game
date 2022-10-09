f96707d23329ade4e61acf23ea3e40ad57c97bfc
syltur <sylwia.tur99@gmail.com>
2022-10-09 14:46:06 +0200

    add variables

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,6 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+let computerMove = `kamień`;
+let playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove+ ', to wygrywasz!');
