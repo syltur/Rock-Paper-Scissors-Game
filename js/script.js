9f8b0f0c126a60387851755e9179558cf58d32ab
syltur <sylwia.tur99@gmail.com>
2022-10-10 17:50:09 +0200

    add player move

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ if (randomNumber == 1){
 	printMessage('Ruch komputera to : ' + computerMove);
 }
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+  printMessage('Twój ruch to: ' + playerMove);
+} else if (playerInput == 2) {
+	playerMove = 'nożyczki';
+	printMessage('Twój ruch to: ' + playerMove);
+} else if (playerInput == 3) {
+	playerMove = 'nożyczki';
+	printMessage('Ruch komputera to : ' + playerMove);
+}
+
