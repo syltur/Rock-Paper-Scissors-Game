e73b7a7a572564e91555beb59a44c35fcadfb03e
syltur <sylwia.tur99@gmail.com>
2022-10-10 21:02:28 +0200

    update function result

--- js/script.js
+++ js/script.js
@@ -26,8 +26,6 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
 if(playerInput == 1){
   playerMove = 'kamień';
   printMessage('Twój ruch to: ' + playerMove);
@@ -38,11 +36,22 @@ if(playerInput == 1){
 	playerMove = 'nożyce';
 	printMessage('Ruch komputera to : ' + playerMove);
 }
-if(playerMove === 'kamień' && computerMove === "kamień"){
-    printMessage('Remis ');
-} else if (playerMove === 'kamień' && computerMove === "papier"){
+if(playerMove === 'kamień' && computerMove === 'kamień'){
+    printMessage('Remis '); 
+}else if (playerMove === 'papier' && computerMove === 'papier'){
+    printMessage('Remis '); 
+}else if (playerMove === 'nożyce' && computerMove === 'nożyce'){
+    printMessage('Remis '); 
+} else if (playerMove === 'kamień' && computerMove === 'papier'){
     printMessage('Przykro mi, przegrałeś rozgrywkę. ');
-} else if (playerMove === 'kamień' && computerMove === "nożyce"){
+}else if (playerMove === 'kamień' && computerMove === 'nożyce'){
     printMessage('Gratuluję! Wgrałeś rozgrywkę. ');
+}else if (playerMove === 'nożyce' && computerMove === 'papier'){
+    printMessage('Gratuluję! Wgrałeś rozgrywkę. ');
+}else if (playerMove === 'nożyce' && computerMove === 'kamień'){
+    printMessage('Przykro mi, przegrałeś rozgrywkę. ');
+}else if (playerMove === 'papier' && computerMove === 'kamień'){
+    printMessage('Gratuluję! Wgrałeś rozgrywkę. ');
+}else if (playerMove === 'papier' && computerMove === 'nożyce'){
+    printMessage('Przykro mi, przegrałeś rozgrywkę. ');   
 }
-
